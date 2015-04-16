92505aea64daf27288b182f67873a83ef178b289
carlosgsouza <carlosgsouza@gmail.com>
2015-04-15 23:53:12 -0700

    Introducing non working algorithm for dinning philosophers

---
+++ src/concurrency/DinningPhilosophers.groovy
@@ -0,0 +1,80 @@
+package concurrency
+
+import spock.lang.Specification
+
+class DinningPhilosophers extends Specification {
+	
+	int seats = 2
+	def forks = 0..<seats
+	boolean dinnerIsOver = false
+	
+	def "all philosophers should eat about the same amount"() {
+		given: "the philosophers"
+		List<Philosopher> philosophers = []
+		seats.times{ 
+			philosophers << new Philosopher(it, it, (it+1)%seats)
+		}
+		
+		when: "you tell them to start eating"
+		philosophers.each { 
+			it.start()
+		}
+		
+		and: "wait a little bit and tell them dinner is over"
+		sleep(1000)
+		dinnerIsOver = true
+
+		def totalEaten = 0
+		philosophers.each {
+			println it;
+			totalEaten += it.bites
+		}
+				
+		then: "they will have eaten"
+		totalEaten > 0
+		
+		and: "none of them ate too much or too little"
+		int minimumEaten = (totalEaten / seats) * 0.75
+		int maximumEaten = (totalEaten / seats) * 1.25
+		
+		philosophers*.bites.every{ it > minimumEaten }
+		philosophers*.bites.every{ it < maximumEaten }
+		
+	}
+	
+	class Food {
+		int remainingBites
+	}
+	
+	class Philosopher extends Thread {
+		int leftFork
+		int rightFork
+		int number
+		int bites
+				
+		Philosopher(number, leftFork, rightFork) {
+			this.number = number
+			this.leftFork = leftFork
+			this.rightFork = rightFork
+		}
+		
+		void run() {
+			while(dinnerIsOver == false) {
+				synchronized(forks[leftFork]) {
+					println "Philosopher $number grabbed fork $leftFork"
+					synchronized(forks[rightFork]) {
+						println "Philosopher $number grabbed fork $rightFork"
+						println "Philosopher $number ate"
+						bites++
+					}
+				}
+			}
+		}
+		
+		String toString() {
+			"Philosopher $number ate $bites bites"
+		}
+		
+	}
+
+}
